f45f8a0885570cf0c511ad6ac93bb4dc3e18b289
AngelSmithlgs <angelsmithlgs@gmail.com>
2024-02-20 18:27:30 -0300

    añado iconos y reglas de colores

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import pizzalogo from '../../img/pizzalogo.png';
+import UserLog from '../../img/user.svg';
+import Cart from '../../img/cart.svg';
 
 const Header = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
+         <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="#home" className="mx-auto">
             <img src={pizzalogo} alt='Logo' style={{ maxWidth: '150px' }} />
@@ -20,13 +22,25 @@ const Header = () => {
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="roboto-bold mr-auto" style={{ margin: 'auto' }}>
+              <Nav.Link href="#inicio">Inicio</Nav.Link>
+              <Nav.Link href="#promociones">Promociones</Nav.Link>
+              <Nav.Link href="#menu">Menú</Nav.Link>
+              <Nav.Link href="#zona-de-reparto">Zona de reparto</Nav.Link>
+              <Nav.Link href="#quienes-somos">Quienes Somos</Nav.Link>
+            </Nav>
             <Nav className="roboto-bold">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <Nav.Link href="#order">Pedir en línea</Nav.Link>
-              <Nav.Link href="#promotions">Promociones</Nav.Link>
-              <Nav.Link href="#user">Quienes Somos</Nav.Link>
-              <Nav.Link href="#cart">Contacto</Nav.Link>
+            <Nav.Item>
+              <Nav.Link href="#login" className="iconlogo">
+                <img src={UserLog} alt='Login' style={{ maxWidth: '50px' }} />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#cart" className="iconlogo">
+                <img src={Cart} alt='cart' style={{ maxWidth: '50px' }} />
+              </Nav.Link>
+            </Nav.Item>
             </Nav>
           </Navbar.Collapse>
         </Container>
