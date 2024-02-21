1ea3518159846cd16931657c5fcad5b6c2044d53
AngelSmithlgs <angelsmithlgs@gmail.com>
2024-02-20 23:53:37 -0300

    fix

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCCL2d2DjIvAPcFIB5qIc2S_3KEBBFR4ac",
-  authDomain: "proyecto-pizza-8a014.firebaseapp.com",
-  projectId: "proyecto-pizza-8a014",
-  storageBucket: "proyecto-pizza-8a014.appspot.com",
-  messagingSenderId: "1007728884962",
-  appId: "1:1007728884962:web:4d919e4d9ddd0e5405f2bb"
+  apiKey: "AIzaSyBx8VImExB7MvP4hzIdUFsttr1qXV7M_hk",
+  authDomain: "pizza-29738.firebaseapp.com",
+  projectId: "pizza-29738",
+  storageBucket: "pizza-29738.appspot.com",
+  messagingSenderId: "43677917383",
+  appId: "1:43677917383:web:5099f66461b21d5a849ee2",
+  measurementId: "G-FPXQ86Y0XN"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
+const auth = getAuth(app)
+
+
+
+export { app, auth };
